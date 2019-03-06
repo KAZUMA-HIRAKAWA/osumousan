f3504577b77aab1df083c2f86534c557a3f4935a
西村彩希 <>
2019-03-06 12:38:42 +0900

    no message

---
+++ vue-template/src/components/officeselect.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="office">
+    <h1>どちらの座席表をみますか？</h1>
+    <b-container>
+    <b-form-group label="">
+      <b-form-radio-group
+        id="btnradios"
+        buttons
+        button-variant="outline-success"
+        size="lg"
+        v-model="office"
+        :options="options"
+        name="radioBtnOutline"
+      />
+    </b-form-group>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      seat: '',
+      office: '',
+      options: [
+          { text: '銀座', value: 'ginza' },
+          { text: '丸ノ内', value: 'maru' }
+        ]
+    }
+  },
+  methods: {
+    routing: function(page) {
+      this.$router.push(page)
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+#btnradios{
+  margin-top: 45px;
+}
+</style>
